cdcaca3f2b8dad50f169c46a6e161101af30ff92
Jackson Argo <ignition.argo@gmail.com>
2017-04-20 13:44:22 -0500

    Updated testing for slack controller

--- spec/slack_controller_spec.rb
+++ spec/slack_controller_spec.rb
@@ -19,6 +19,10 @@ describe 'The CloudBurrito controller' do
   let (:controller) { SlackController.new params }
 
   context '#initialize' do
+    it 'creates dne patron' do
+      SlackController.new "user_id" => '2'
+      expect(Patron.last).to eq Patron.find('2')
+    end
     it 'set params' do
       expect(controller.params).to eq(params)
     end
@@ -30,26 +34,64 @@ describe 'The CloudBurrito controller' do
     end
   end
 
+  context '#status' do
+    it 'knows when there isnt a burrito' do
+      expect(controller.status).to eq "You don't have any in coming burritos."
+    end
+    it 'knows when delivery isnt acked' do
+      Package.create hungry_man: patron
+      expect(controller.status).to eq "You burrito is still in the fridge."
+    end
+    it 'knows when burrito should be coming' do
+      Package.create hungry_man: patron, en_route: true
+      expect(controller.status).to eq "This burrito is on it's way!"
+    end
+  end
+
   context '#feed' do
     let(:other) { Patron.create user_id: "2" }
-    it 'patron must be active' do
-      controller.patron.is_active = false
-      expect(controller.feed).to eq("Please join the pool.")
+    context 'patron cannot feed' do
+      it 'patron must be active' do
+        controller.patron.is_active = false
+        expect(controller.feed).to eq("Please join the pool.")
+      end
+      it 'patron cant be on delivery' do
+        patron.active!
+        Package.create hungry_man: other, delivery_man: patron
+        expect(controller.feed).to eq("*You* should be delivering a burrito!")
+      end
+      it 'patron cant be waiting' do
+        patron.active!
+        Package.create hungry_man: patron, delivery_man: other
+        expect(controller.feed).to eq("You already have a burrito coming!")
+      end
+      it 'patron cant be greedy' do
+        patron.active!
+        expect(patron.greedy?).to be true
+        expect(controller.feed).to eq("Stop being so greedy! Wait #{patron.time_until_hungry}s.")
+      end
     end
-    it 'patron cant be on delivery' do
-      patron.active!
-      Package.create hungry_man: other, delivery_man: patron
-      expect(controller.feed).to eq("*You* should be delivering a burrito!")
-    end
-    it 'patron cant be waiting' do
-      patron.active!
-      Package.create hungry_man: patron, delivery_man: other
-      expect(controller.feed).to eq("You already have a burrito coming!")
-    end
-    it 'patron cant be greedy' do
-      patron.active!
-      expect(patron.greedy?).to be true
-      expect(controller.feed).to eq("Stop being so greedy! Wait #{patron.time_until_hungry}s.")
+    context 'patron can feed' do
+      before(:each) do
+        patron.active!
+        patron.force_not_greedy = true
+        patron.save
+      end
+      it 'tells patron we are preparing his burrito' do
+        expect(controller.feed).to eq("Our chefs are hard at work to prepare your burrito!")
+      end
+      it 'creates a burrito for patron' do
+        controller.feed
+        expect(Package.count).to eq 1
+      end
+      it 'patron is now waiting for burrito' do
+        controller.feed
+        expect(patron.waiting?).to be true
+      end
+      it 'patron incoming burrito matches the new burrito' do
+        controller.feed
+        expect(patron.incoming_burrito).to eq Package.first
+      end
     end
   end
 
@@ -82,152 +124,67 @@ describe 'The CloudBurrito controller' do
 
   context '#serving' do
     let (:other)  { Patron.create user_id: "2" }
-    it 'checks if patron is delivering a burrito' do
-      expect(controller.serving).to eq("You aren't on a delivery...")
+    context 'patron is not on a delivery' do
+      it 'tells them they arent delivering' do
+        expect(controller.serving).to eq("You haven't been volunteered to deliver...")
+      end
     end
-    it 'check if a package has already been acked' do
-      Package.create en_route: true, hungry_man: other, delivery_man: patron
-      expect(controller.serving).to eq("You've already acked this request...")
+    context 'package is already acked' do
+      it 'tells patron this package is acked' do
+        Package.create en_route: true, hungry_man: other, delivery_man: patron
+        expect(controller.serving).to eq("You've already acked this request...")
+      end
     end
-    it 'marks package as en_route' do
-      p = Package.create hungry_man: other, delivery_man: patron
-      expect(controller.serving).to eq("Make haste!")
-      p.reload
-      expect(p.en_route).to be true
+    context 'patron needs to ack' do
+      before(:each) do
+        Package.create hungry_man: other, delivery_man: patron
+      end
+      it 'tells patron to make haste' do
+        expect(controller.serving).to eq("Make haste!")
+      end
+      it 'marks package as en_route' do
+        controller.serving
+        package = Package.first
+        expect(package.en_route).to be true
+      end
     end
   end
 
   context '#full' do
-    let (:other)  { Patron.create user_id: "2" }
-    it 'checks if patron has incoming burritos' do
-      expect(controller.full).to eq("You don't have any in coming burritos")
+    let (:dman)  { Patron.create user_id: "2" }
+    context 'patron does not have incoming burritos' do
+      it 'tells them to order a burrito' do
+        expect(controller.full).to eq("You don't have any incoming burritos. Order one with: */cloudburrito feed*")
+      end
     end
-    it 'marks package as received' do
-      p = Package.create hungry_man: patron, delivery_man: other
-      expect(controller.full).to eq("Enjoy!")
-      p.reload
-      expect(p.received).to be true
+    context 'patron has incoming burritos' do
+      before(:each) do
+        Package.create hungry_man: patron, delivery_man: dman
+      end
+      it 'tells patron to enjoy' do
+        expect(controller.full).to eq "Enjoy!"
+      end
+      it 'marks package as received' do
+        controller.full
+        package = Package.first
+        expect(package.received).to be true
+      end
+      it 'patron is no longer waiting' do
+        controller.full
+        expect(patron.waiting?).to be false
+      end
+      it 'patron is now greedy' do
+        controller.full
+        expect(patron.greedy?).to be true
+      end
+      it 'delivery man is no longer on delivery' do
+        controller.full
+        expect(dman.on_delivery?).to be false
+      end
+      it 'delivery man is now sleep' do
+        controller.full
+        expect(dman.sleepy?).to be true
+      end
     end
-  end
-
-  def create_burrito_and_patrons
-    # There shoudn't be any patrons or burritos
-    expect(Patron.count).to eq(0)
-    expect(Package.count).to eq(0)
-    # Create two patrons
-    c = SlackController.new "user_id" => '1'
-    c.join
-    c = SlackController.new "user_id" => '2'
-    c.join
-    expect(Patron.count).to eq(2)
-    p1 = Patron.find('1')
-    p2 = Patron.find('2')
-    # Make p1 not greedy
-    p1.force_not_greedy = true
-    p1.save
-    expect(p1.greedy?).to eq(false)
-    # Create a burrito for p1
-    c = SlackController.new "user_id" => '1'
-    c.feed
-    p1.reload
-    p2.reload
-    expect(Package.count).to eq(1)
-    b = Package.first
-    expect(p1.waiting?).to eq(true)
-    expect(p1.incoming_burrito).to eq(b)
-    expect(p2.on_delivery?).to eq(true)
-    expect(p2.active_delivery).to eq(b)
-    expect(b.en_route).to eq(false)
-    expect(b.received).to eq(false)
-    return p1, p2, b
-  end
-
-  it "Can create patrons" do
-    (1..10).each do |x| 
-      c = SlackController.new("user_id" => x.to_s)
-      c.join
-    end
-    expect(Patron.count).to eq(10)
-    Patron.each do |p| 
-      expect(p.is_active).to eq(true)
-      expect(p.greedy?).to eq(true)
-    end
-  end
-
-  it "Won't create a second patron with same id" do
-    (1..10).each do 
-      c = SlackController.new("user_id" => '1')
-      c.join
-    end
-    expect(Patron.count).to eq(1)
-  end
-
-  it "can create burritos" do
-    create_burrito_and_patrons
-  end
-
-  it "can check the status of burritos" do
-    p1, _p2, _b = create_burrito_and_patrons
-    c = SlackController.new "user_id" => p1.user_id
-    c.status
-  end
-
-  it "Hungryman can ack and Delivery can ack" do
-    p1, p2, b = create_burrito_and_patrons
-    # p2 should ack that he's on delivery
-    c = SlackController.new "user_id" => '2'
-    c.serving
-    p1.reload
-    p2.reload
-    b.reload
-    expect(b.en_route).to eq(true)
-    expect(b.received).to eq(false)
-    expect(p1.waiting?).to eq(true)
-    expect(p2.on_delivery?).to eq(true)
-    # p1 should ack that he received the burrito
-    c = SlackController.new "user_id" => '1'
-    c.full
-    p1.reload
-    p2.reload
-    b.reload
-    expect(b.en_route).to eq(true)
-    expect(b.received).to eq(true)
-    expect(p1.waiting?).to eq(false)
-    expect(p2.on_delivery?).to eq(false)
-    p1.force_not_greedy = false
-    p1.save
-    expect(p1.greedy?).to eq(true)
-    expect(p2.sleepy?).to eq(true)
-  end
-
-  it "will look for a new delivery man if package goes stale" do
-    p1, p2, b = create_burrito_and_patrons
-    # Create a new patron to deliver
-    c = SlackController.new "user_id" => '3'
-    c.join
-    p3 = Patron.find '3'
-    # p2 doesn't ack
-    b.force_stale = true
-    b.save
-    # Monitoring determines package is stale
-    x = Time.now
-    while(not b.failed or (Time.now - x).to_f > 1) do
-      b.reload
-    end
-    p2.reload
-    expect(b.failed).to eq(true)
-    expect(p2.is_active).to eq(false)
-    # A new package is created
-    x = Time.now
-    while(not b.retry or (Time.now - x).to_f > 1) do
-      b.reload
-    end
-    expect(b.retry).to eq(true)
-    expect(Package.count).to eq(2)
-    b = Package.last
-    expect(b.en_route).to eq(false)
-    expect(b.received).to eq(false)
-    expect(p1.waiting?).to eq(true)
-    expect(p3.on_delivery?).to eq(true)
   end
 end
