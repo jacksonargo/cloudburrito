b18f38aad36c1e34b7de742662a5f8bbba0650eb
Jackson Argo <ignition.argo@gmail.com>
2017-04-22 00:53:45 -0500

    Logger write to stdout if log file not rw

--- lib/cloudburrito_logger.rb
+++ lib/cloudburrito_logger.rb
@@ -7,8 +7,13 @@ require 'fileutils'
 # A module that add a logger method.
 module CloudBurritoLogger
   def logger
+    logfile = 'log/app.log'
     FileUtils.mkdir_p 'log'
-    @__logger ||= Logger.new('log/app.log')
+    if File.writable? logfile
+      @__logger ||= Logger.new('log/app.log')
+    else
+      @__logger ||= Logger.new(STDOUT)
+    end
     @__logger
   end
 end
